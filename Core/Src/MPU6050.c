4279d912b5295887aa205750b13f38128286148f
Arsen <71315641+arersen@users.noreply.github.com>
2024-11-20 11:23:05 +0200

    Update MPU6050.c

--- Core/Src/MPU6050.c
+++ Core/Src/MPU6050.c
@@ -8,6 +8,14 @@
 
 #include "MPU6050.h"
 
+typedef struct{
+	double x, y, z;
+} Vector3;
+
+typedef struct{
+	uint16_t x, y, z;
+} u16vec3;
+
 I2C_HandleTypeDef* hi2c;
 
 void MPU6050_Write(uint16_t MemAddress, uint8_t *pData, uint16_t Size){
@@ -22,7 +30,7 @@ void MPU6050_Init(I2C_HandleTypeDef* hi2c_target){
 	uint8_t check, data;
 	hi2c = hi2c_target;
 
-	MPU6050_Read(WHO_AM_I_REG, &check);
+	MPU6050_Read(WHO_AM_I_REG, &check, 1);
 	if(check == 104){
 		data = 0;
 		MPU6050_Write(PWR_MGMT_1_REG, &data, 1);
@@ -42,26 +50,25 @@ void MPU6050_Init(I2C_HandleTypeDef* hi2c_target){
 
 }
 
-void MPU6050_Read_Accel(Vector3* pAccelRAW){
-	uint8_t Rec_Data[6];
 
-	MPU6050_Read(ACCEL_XOUT_H_REG, Rec_Data, 6); // sizeof(Rec_Data)
+void MPU6050_Read_Accel(Vector3* pAccel){
+	u16vec3 pAccelRAW;
 
-	pAccelRAW->x = (uint16_t)(Rec_Data[0] << 8 | RecData[1]);
-	pAccelRAW->y = (uint16_t)(Rec_Data[2] << 8 | RecData[3]);
-	pAccelRAW->z = (uint16_t)(Rec_Data[4] << 8 | RecData[5]);
-	
+	MPU6050_Read(ACCEL_XOUT_H_REG, (uint8_t*)&pAccelRAW, sizeof(pAccelRAW));
+
+	pAccel->x = (double)pAccelRAW.x / 16384.0;
+	pAccel->y = (double)pAccelRAW.y / 16384.0;
+	pAccel->z = (double)pAccelRAW.z / 16384.0;
 
 }
 
-void MPU6050_Read_Gyro(Vector3* pGyroRAW){
-	uint8_t Rec_Data[6];
+void MPU6050_Read_Gyro(Vector3* pGyro){
+	u16vec3 pGyroRAW;
 
-	MPU6050_Read(GYRO_XOUT_H_REG, Rec_Data, 6); // sizeof(Rec_Data)
+	MPU6050_Read(GYRO_XOUT_H_REG, (uint8_t*)&pGyroRAW, sizeof(pGyroRAW));
 
-	pGyroRAW->x = (uint16_t)(Rec_Data[0] << 8 | RecData[1]);
-	pGyroRAW->y = (uint16_t)(Rec_Data[2] << 8 | RecData[3]);
-	pGyroRAW->z = (uint16_t)(Rec_Data[4] << 8 | RecData[5]);
-	
+	pGyro->x = (double)pGyroRAW.x / 131.0;
+	pGyro->y = (double)pGyroRAW.y / 131.0;
+	pGyro->z = (double)pGyroRAW.z / 131.0;
 
 }
